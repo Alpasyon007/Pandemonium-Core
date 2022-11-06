b150172521a1461e7f290bf6a00f83f7ca1f00a8
Alpasyon007 <alpasyon07@gmail.com>
2022-11-06 04:06:58 +0000

    Added Layer Support

--- headers/Application.h
+++ headers/Application.h
@@ -13,18 +13,23 @@ namespace Pandemonium {
 		Application();
 		virtual ~Application();
 
-		void Run();
+		void					   Run();
 
-		void OnEvent(Event& e);
+		void					   OnEvent(Event& e);
 
-		void PushLayer(Layer* layer);
-		void PushOverlay(Layer* layer);
+		void					   PushLayer(Layer* layer);
+		void					   PushOverlay(Layer* layer);
+
+		inline static Application& Get() { return *s_Instance; }
+		inline Window&			   GetWindow() { return *m_Window; }
 	private:
 		bool					OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool					m_Running = true;
 		LayerStack				m_LayerStack;
+	private:
+		inline static Application* s_Instance = nullptr;
 	};
 
 	Application* CreateApplication();
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,16 +1,26 @@
 #include "Application.h"
 
 namespace Pandemonium {
+
 	Application::Application() {
-		m_Window = std::unique_ptr<Window>(Window::Create());
+		ASSERT(!s_Instance, "Application already exists!");
+		s_Instance = this;
+
+		m_Window   = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
 	}
 
 	Application::~Application() {}
 
-	void Application::PushLayer(Layer* layer) { m_LayerStack.PushLayer(layer); }
+	void Application::PushLayer(Layer* layer) {
+		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
+	}
 
-	void Application::PushOverlay(Layer* layer) { m_LayerStack.PushOverlay(layer); }
+	void Application::PushOverlay(Layer* layer) {
+		m_LayerStack.PushOverlay(layer);
+		layer->OnAttach();
+	}
 
 	void Application::OnEvent(Event& e) {
 		EventDispatcher dispatcher(e);
@@ -41,4 +51,5 @@ namespace Pandemonium {
 		m_Running = false;
 		return true;
 	}
+
 } // namespace Pandemonium
