69dd1d28905a73b4b868b4ae8ff6023a4ba82ff2
Alpasyon007 <alpasyon07@gmail.com>
2022-11-15 04:45:02 +0000

    Implement Basic Texture Support

--- headers/Core.h
+++ headers/Core.h
@@ -1,6 +1,8 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include <memory>
+
 // #include "PandemoniumMath.h"
 #include "PandemoniumLog.h"
 
@@ -13,5 +15,13 @@
 #else
 	#error NO PLATFORMS DEFINED
 #endif
+
+namespace Pandemonium {
+
+	template <typename T> using Scope = std::unique_ptr<T>;
+
+	template <typename T> using Ref	  = std::shared_ptr<T>;
+
+} // namespace Pandemonium
 
 #endif /* CORE_H */
