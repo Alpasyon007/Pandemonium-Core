c901d4f63331f52bb8ca6f7c99c299007743ebb4
Alpasyon007 <alpasyon07@gmail.com>
2022-12-03 05:00:38 +0000

    Add profiling

--- headers/EntryPoint.h
+++ headers/EntryPoint.h
@@ -8,9 +8,17 @@
 	extern Pandemonium::Application* Pandemonium::CreateApplication();
 
 	int main(int argc, char** argv) {
+		PANDEMONIUM_PROFILE_BEGIN_SESSION("Startup", "PandemoniumProfile-Startup.json")
 		Pandemonium::Application* app = Pandemonium::CreateApplication();
+		PANDEMONIUM_PROFILE_END_SESSION();
+
+		PANDEMONIUM_PROFILE_BEGIN_SESSION("Startup", "PandemoniumProfile-Runtime.json")
 		app->Run();
+		PANDEMONIUM_PROFILE_END_SESSION();
+
+		PANDEMONIUM_PROFILE_BEGIN_SESSION("Startup", "PandemoniumProfile-Shutdown.json")
 		delete app;
+		PANDEMONIUM_PROFILE_END_SESSION();
 	}
 #endif
 // clang-format on
